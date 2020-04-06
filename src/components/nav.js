a11c736503dc7b2ccef387d1547ecee8c2a63066
Kiara Wu <kiarapwu@gmail.com>
2020-04-06 00:15:07 -0700

    added files

---
+++ src/components/nav.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+function Nav() => {
+<nav className="navbar navbar-expand navbar-light bg-light">
+<div className="navbar-collapse row" id="navbarNav">
+
+</div>
+</nav>
+}
---
+++ src/components/search.js
@@ -0,0 +1,13 @@
+import react from "react";
+
+function searchinput () => {
+    <div className="searchinput">
+    <form className="form-inline">
+      <input
+        className="form-control"
+        type="search"
+        placeholder="Search"
+      />
+    </form>
+  </div>
+}
